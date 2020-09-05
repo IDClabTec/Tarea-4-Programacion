1880a6fce4a0302ad6078359817941d3825a8d33
idclab <idclab.tec@gmail.com>
2020-09-05 18:58:22 -0400

    Cambio Registrado

---
+++ operaciones.py
@@ -0,0 +1,9 @@
+#operaciones
+a=3;b=5;c=4;d=3;
+af=2.5;bf=3.24;cf=6.32;df=1.54;
+#suma;
+suma=a+d;
+print(f'la suma entre a y d es\n>>{a}+{d} = {suma}\nambos son int, el resultado es un int');
+#resta
+resta=bf-df;
+print(f'la resta entre bf y df es\n>>{bf}-{df} = {resta}\naf es un float mientras b es un int, el resultado es un float');
