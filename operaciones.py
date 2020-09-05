3d1830e6020b56c9758bef1546a6a3967ee0ce10
idclab <idclab.tec@gmail.com>
2020-09-05 19:01:29 -0400

    Otro cambio Registrado

--- operaciones.py
+++ operaciones.py
@@ -7,3 +7,12 @@ print(f'la suma entre a y d es\n>>{a}+{d} = {suma}\nambos son int, el resultado 
 #resta
 resta=bf-df;
 print(f'la resta entre bf y df es\n>>{bf}-{df} = {resta}\naf es un float mientras b es un int, el resultado es un float');
+#division
+division=c/df;
+print(f'la division entre c y df es\n>>{c}/{df}={division}\nArroja todos los valores correspondientes y el resultado es un float \nEn caso de ser division entera:\n>>{c}//{df} = {c//df}\nEl resultado sigue siendo un float');
+#multiplicacion
+multiplicacion=d*cf;
+print(f'la multiplicacion entre d y cf es\n>>{d}*{cf} = {multiplicacion}');
+#modulo
+modulo=a%bf;
+print(f'el modulo entre a y bf es\n>>{a}%{bf} = {modulo}');
