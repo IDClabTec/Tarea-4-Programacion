2b3d73f0bc10d1b934f499898784c4f5cde443a5
idclab <idclab.tec@gmail.com>
2020-09-05 19:08:14 -0400

    Creamos los cursos

--- cursos.py
+++ cursos.py
@@ -1,1 +1,10 @@
 #cursos
+alumno1 = {'Nombre': 'Miguel Bosse','Edad':64,'Notas': [{'Asignatura':'Programacion','notas':[17,23]},{'Asignatura':'Fisica', 'notas':[34,21]}]};
+
+alumno2 = {'Nombre': 'Juan Gabriel','Edad':66,'Notas': [{'Asignatura':'Programacion','notas':[70,45]},{'Asignatura':'Fisica', 'notas':[30,50]}]};
+
+alumno3 = {'Nombre': 'Luis Miguel','Edad':50,'Notas': [{'Asignatura':'Programacion','notas':[58,40]},{'Asignatura':'Fisica', 'notas':[46,65]}]};
+
+alumno4 = {'Nombre': 'Chayanne','Edad':52,'Notas': [{'Asignatura':'Programacion','notas':[70,70]},{'Asignatura':'Fisica', 'notas':[70,70]}]};
+
+curso={'Carrera': 'Fisica','semestre':'2do semestre 2020', 'alumnos': [alumno1,alumno2,alumno3,alumno4]};
